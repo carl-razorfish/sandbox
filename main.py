cbbeae827b1e44b279a61cab6aa8b1f7b49d1a8f
Geoff Kaile <geoff.kaile@gmail.com>
2011-06-23 17:31:11 +0100

    GPK - Modified code for pulling in API data

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+feeds = {
+    'bbc'      : 'http://news.bbc.co.uk/weather/forecast/',
+    'guardian' : 'http://content.guardianapis.com/search?format=json&use-date=last-modified&show-fields=headline,trailText&ids=',
+    'yahoo'    : 'http://query.yahooapis.com/v1/public/yql?format=json&q='
+}
+
 class Newyork(webapp.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__),'templates/mashup.html')
         vars = {
             'name'     : 'New York',
             'bbc'      : '101',
-            'guardian' : 'travel/newyork'
+            'news'     : feeds['guardian']+'travel/newyork',
+            'weather'  : feeds['yahoo']+'select * from weather.forecast where location=10118',
         }
         self.response.out.write(template.render(path,vars))
 
