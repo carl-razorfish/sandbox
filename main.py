257346fdf149f100e5c8e1a0077f522834b85fdc
davelaser <stuart.thorne@razorfish.com>
2011-07-05 12:26:22 +0100

    Modified Mashup and RestAPI handlers to accept any destination, and any destination with valid xml or json contetn path, respectively

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+requestRestApi = r"/(.*)/(xml|json)"
+requestUI = r"/(.*)"
+
 feeds = {
     'bbc'      : 'http://news.bbc.co.uk/weather/forecast/',
     'guardian' : 'http://content.guardianapis.com/search?format=json&use-date=last-modified&show-fields=headline,trailText&ids=',
@@ -59,48 +62,52 @@ def all(element, nodename):
     return element.findall(path)
 
 class RestAPI(webapp.RequestHandler):
-    def get(self):
-        
-        r = split_path(self.request.path)
-         
-        if r[1] == 'newyork':
+    def get(self, destination, responseType):
+        logging.info(destination)
+        logging.info(responseType)
+        feed = None 
+        if destination == 'newyork':
             feed = ny_hotels_feed
-        
-        if r[2] == 'json':
-            out = getJSON(feed)
-        elif r[2] == 'xml':
-            out = getXML(feed)
-        
-        self.response.out.write(out)
+        if feed is not None:
+	        if responseType == 'json':
+	            out = getJSON(feed)
+	        elif responseType == 'xml':
+	   			out = getXML(feed)
+	        if out is not None:
+	        	self.response.out.write(out)
 
 class Mashup(webapp.RequestHandler):
-    def get(self):
-        
+    def get(self, destination):
         r = split_path(self.request.path)
-        
+        flights_feed = None
+        hotels_feed = None
+        city_breaks_feed = None
+        mashup = None
+        f = None
+
         if r[1] == '' or r[1] == 'newyork':
             mashup = ny_mashup
             hotels_feed = ny_hotels_feed
             city_breaks_feed = ny_city_breaks_feed
             flights_feed = ny_flights_feed
-        
-        f = parseXML(flights_feed)
-        
-        mashup['hotels'] = parseXML(hotels_feed);
-        mashup['city_break'] = parseXML(city_breaks_feed)[0]
-        mashup['cheapest_flight'] = f[-1]
-        mashup['all_flights'] = f[0:-2]
-        
-        logging.info(mashup['all_flights'])
+        if flights_feed is not None:
+        	f = parseXML(flights_feed)
+        if hotels_feed is not None:
+        	mashup['hotels'] = parseXML(hotels_feed)
+		if city_breaks_feed is not None:
+			mashup['city_break'] = parseXML(city_breaks_feed)[0]
+		if f is not None:
+			mashup['cheapest_flight'] = f[-1]
+        	mashup['all_flights'] = f[0:-2]
+        if mashup is not None:
+			logging.info(mashup['all_flights'])
         
         path = os.path.join(os.path.dirname(__file__),'templates/mashup.html')
-        self.response.out.write(template.render(path, ny_mashup))
+        self.response.out.write(template.render(path, mashup))
 
 application = webapp.WSGIApplication([
-        ('/newyork/xml', RestAPI),
-        ('/newyork/json', RestAPI),
-        ('/newyork',Mashup),
-        ('/',Mashup)
+        (requestRestApi, RestAPI),
+        (requestUI,Mashup)
     ],debug=True)
 
 if __name__ == '__main__':
