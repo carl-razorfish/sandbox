b821d05d04407335b7bda9ede4e3167b9bd955fd
davelaser <stuart.thorne@razorfish.com>
2011-07-06 16:42:11 +0100

    Added google static maps api to hotels

--- static/js/mashup.js
+++ static/js/mashup.js
@@ -8,9 +8,10 @@ RIA = {
         }
     },
     Class : {
-        Article : function() {
+        Article : function() { 
+			
             this.article = arguments[0];
-
+            if(!this.article.dataset) return;
             this.init = function() {
                 this.feed = this.article.dataset['feed'];
                 if (!this.feed) return;
@@ -49,3 +50,15 @@ function news(data) {
     }
     RIA.fn.populateContainer('news',html);
 }
+
+
+document.getElements(".show-map").each(function(mapLink) {  
+
+	mapLink.addEvents({
+		"click": function(e) {
+			e.preventDefault();
+			var map = mapLink.getParent("dl").getElement(".map img"), size = map.getSize();
+			map.getStyle("opacity") > 0 ? map.morph({"opacity":0, "height":"0px"}) : map.morph({"opacity":1, "height":"150px"});
+		}
+	})
+})
